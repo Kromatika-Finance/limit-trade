08495c169aa7284153bb21cfd400186d8d0fb0d1
Panche <pance.isajeski@gmail.com>
2021-12-13 19:18:18 +0100

    deploy monitor to Optimism

---
+++ migrations/4_deploy_limit_order_monitor_op.js
@@ -0,0 +1,23 @@
+const LimitOrderManager = artifacts.require("OpLimitOrderManager");
+const LimitOrderMonitor = artifacts.require("LimitOrderMonitor");
+const Kromatika = artifacts.require("Kromatika");
+const {deployProxy} = require("@openzeppelin/truffle-upgrades");
+
+module.exports = async function (deployer, network, accounts) {
+
+  const uniswapFactory = process.env.UNISWAP_FACTORY;
+  const router = process.env.UNISWAP_ROUTER;
+
+  const limitOrderManagerInstance = await LimitOrderManager.deployed();
+  const kromatikaInstance = await Kromatika.deployed();
+
+  //_maxBatchSize = 10, monitorSize=20, monitorInterval = 1 block,
+  // monitorFee = 20 % (this needs to be in a global config); the same % should be applied in the estimation
+  await deployProxy(LimitOrderMonitor,
+      [limitOrderManagerInstance.address, uniswapFactory, kromatikaInstance.address, accounts[0],
+        10, 100, 1, 20000],
+      {deployer});
+
+  const limitOrderMonitorInstance = await LimitOrderMonitor.deployed()
+  await limitOrderManagerInstance.addMonitor(limitOrderMonitorInstance.address);
+};
