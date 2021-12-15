45a8efe18c76f51058d694e9bc2287f678a50445
Panche <pance.isajeski@gmail.com>
2021-12-15 12:12:17 +0100

    add deploy scripts for different mointor types

---
+++ migrations/4_deploy_limit_order_monitor_chainlink.js
@@ -0,0 +1,25 @@
+const LimitOrderManager = artifacts.require("LimitOrderManager");
+const LimitOrderMonitor = artifacts.require("LimitOrderMonitorChainlink");
+const Kromatika = artifacts.require("Kromatika");
+const {deployProxy} = require("@openzeppelin/truffle-upgrades");
+
+module.exports = async function (deployer, network, accounts) {
+
+  // const uniswapFactory = process.env.UNISWAP_FACTORY;
+  // const router = process.env.UNISWAP_ROUTER;
+  // const linkAddress = process.env.LINK;
+  // const wrappedETHAddress = process.env.WETH;
+  //
+  // const limitOrderManagerInstance = await LimitOrderManager.deployed();
+  // const kromatikaInstance = await Kromatika.deployed();
+  //
+  // //_maxBatchSize = 10, monitorSize=20, monitorInterval = 1 block,
+  // // monitorFee = 20 % (this needs to be in a global config); the same % should be applied in the estimation
+  // await deployProxy(LimitOrderMonitor,
+  //     [limitOrderManagerInstance.address, uniswapFactory, kromatikaInstance.address,
+  //       10, 100, 1, 20000, router, wrappedETHAddress, linkAddress],
+  //     {deployer});
+  //
+  // const limitOrderMonitorInstance = await LimitOrderMonitor.deployed()
+  // await limitOrderManagerInstance.addMonitor(limitOrderMonitorInstance.address);
+};
