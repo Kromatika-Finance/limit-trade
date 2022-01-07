956f7b33d9b9c6a95e00ae04100c3e9670b656f6
Panche <pance.isajeski@gmail.com>
2022-01-07 10:56:03 +0100

    Critical issue: Incorrect amounts on cancel

--- contracts/LimitOrderManager.sol
+++ contracts/LimitOrderManager.sol
@@ -334,8 +334,8 @@ contract LimitOrderManager is
             IUniswapV3Pool(limitOrder.pool),
             limitOrder.tickLower,
             limitOrder.tickUpper,
-            limitOrder.tokensOwed0,
-            limitOrder.tokensOwed1,
+            _amount0,
+            _amount1,
             msg.sender
         );
 
